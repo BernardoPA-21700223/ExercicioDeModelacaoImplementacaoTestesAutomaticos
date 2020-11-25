e7423e1e4e5a8dc675cc9aaf649b41fb927d83e3
BernardoPA-21700223 <a21700223@alunos.ulht.pt>
2020-11-25 17:34:10 +0000

    a

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Funcionario> funcionarios = new ArrayList<>();
+
+        funcionarios.add(new Funcionario("bernardo"));
+        funcionarios.add(new Funcionario("joao"));
+
+
+        funcionarios.get(0).adicionaTarefa(new Tarefa("entrar cedo",8, new Date(25-11-2020)));
+
+    }
+
 }
--- src/Tarefa.java
+++ src/Tarefa.java
@@ -1,2 +1,18 @@
+import java.util.Date;
+
 public class Tarefa {
+
+    String dicriçao;
+    int horas;
+    Date data;
+
+    public Tarefa(String dicriçao, int horas, Date data) {
+        this.dicriçao = dicriçao;
+        this.horas = horas;
+        this.data = data;
+    }
+
+
+
+
 }
